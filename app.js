a0aa27fc4a163836aebf6fcee7168f43647de33d
christie <cbrandao18@gmail.com>
2019-07-09 13:54:28 -0400

    Minor tweaks to nav bar

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
