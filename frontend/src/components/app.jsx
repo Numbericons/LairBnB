9692da8dd76153219d91af68cc2725e2cc952ea1
christie <cbrandao18@gmail.com>
2019-07-09 12:29:13 -0400

    get splash page to render

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -4,8 +4,8 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
 import Splash from './splash';
-import SignupFormContainer from '../session/signup_form_container';
-import LoginFormContainer from '../session/login_form_container';
+// import SignupFormContainer from '../session/signup_form_container';
+// import LoginFormContainer from '../session/login_form_container';
 
 const App = () => (
   <section>
@@ -14,8 +14,8 @@ const App = () => (
         {/* <Route path="/lair/:lair_id" component={LairShowContainer} /> */}
         {/* <Route path="/search/all" component={LairIndex} */}
     </Switch>
-    <AuthRoute path="/login" component={LoginFormContainer} />
-    <AuthRoute path="/register" component={SignupFormContainer} />
+    {/* <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/register" component={SignupFormContainer} /> */}
 
 
   </section> 
