b621b36fb2a56a8eb6b751cde84fbe7e5df4ee14
christie <cbrandao18@gmail.com>
2019-07-09 13:29:29 -0400

    Start splash background and top nav

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 import { logout } from './actions/session_actions';
-
+import "./stylesheets/output.css"
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 
