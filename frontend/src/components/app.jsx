af7799f950a5dfca5a58f907bfe00d96271a6839
christie <cbrandao18@gmail.com>
2019-07-11 17:23:10 -0400

    Create footer

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import MainLairIndexContainer from './lairs/main_lairs_index_container';
+import Footer from './footer/footer';
 
 import Splash from './splash';
 // import SignupFormContainer from '../session/signup_form_container';
@@ -16,6 +17,7 @@ const App = () => (
         {/* <Route path="/lair/:lair_id" component={LairShowContainer} /> */}
         {/* <Route path="/search/all" component={LairIndex} */}
     </Switch>
+    <Route exact path="/" component={Footer} />
     {/* <AuthRoute path="/login" component={LoginFormContainer} />
     <AuthRoute path="/register" component={SignupFormContainer} /> */}
 
---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <div className="footer-content">
+                <div className="column">
+                    <h2 className="column-header">
+                        Technologies
+                    </h2>
+                    <ul>
+                        <li>Javascript</li>
+                        <li>MongoDB</li>
+                        <li>Express</li>
+                        <li>React</li>
+                        <li>NodeJS</li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <h2 className="column-header">
+                        Developers
+                    </h2>
+                    <ul>
+                        <li>Christie Brandao</li>
+                        <li>April Graves</li>
+                        <li>Charles Mancuso</li>
+                        <li>Zachary Oliver</li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <h2 className="column-header">
+                        Description
+                    </h2>
+                    <p>This is a full-stack clone of AirBnB created in one week.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
