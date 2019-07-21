3892eb385234fc0681c6863773955f2823b011fc
christie <cbrandao18@gmail.com>
2019-07-21 11:05:45 -0400

    Block out dates on calendar that are already booked

--- frontend/src/components/bookings/reserve_form.jsx
+++ frontend/src/components/bookings/reserve_form.jsx
@@ -5,17 +5,20 @@ import 'react-dates/lib/css/_datepicker.css';
 import { connect } from 'react-redux';
 import LoginFormContainer from '../session/login_form_container';
 import SignupFormContainer from '../session/signup_form_container';
-import {createBooking} from '../../actions/booking_actions';
+import { createBooking, fetchBookings } from '../../actions/booking_actions';
+import moment from 'moment';
 
 const mapStateToProps = (state, ownProps) => {
   return {
     currentUserId: state.session.id,
+    bookings: state.entities.bookings,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    createBooking: booking => dispatch(createBooking(booking))
+    createBooking: booking => dispatch(createBooking(booking)),
+    fetchBookings: () => dispatch(fetchBookings())
   }
 }
 
@@ -36,6 +39,11 @@ class ReserveForm extends React.Component {
         this.switchModal = this.switchModal.bind(this);
         this.reserve = this.reserve.bind(this);
         this.showReservationDetails = this.showReservationDetails.bind(this);
+        this.disableBookedDay = this.disableBookedDay.bind(this);
+    }
+
+    componentDidMount(){
+        this.props.fetchBookings();
     }
 
     switchModal(modalName) {
@@ -134,8 +142,25 @@ class ReserveForm extends React.Component {
         )
     }
 
-    disableBookedDays() {
-        
+    disableBookedDay(day) {
+      let unavailableDays = [];
+      Object.keys(this.props.bookings).forEach(bookingId => {
+        let booking = this.props.bookings[bookingId]
+        if (booking.lair_id === this.state.lair_id) {
+          unavailableDays.push(moment(booking.arrival_date))
+          unavailableDays.push(moment(booking.departure_date))
+
+          let dateToCheck = moment(booking.arrival_date).add(1, 'd');
+          while (dateToCheck.isBefore(moment(booking.departure_date))){
+            unavailableDays.push(dateToCheck);
+            dateToCheck = moment(dateToCheck).add(1, 'd');
+          }
+        }
+      })
+      console.log(unavailableDays);
+      if (unavailableDays.length !== 0){
+        return unavailableDays.some((unavailableDay) => {return unavailableDay.isSame(day)});
+      }
     }
     
     render(){
@@ -143,44 +168,45 @@ class ReserveForm extends React.Component {
       for (let i = 2; i <= 16; i++) {
         number_of_guests.push(<option value={i} key={i} default>{i} guests</option>)
       }
-    const num_months = window.innerWidth <= 700 ? 1 : 2;
-       return (
-           <div className="reserve-form-container">
-               <div className="reserve-form-details">
-                   <span className="reserve-form-rate">${this.props.lair.rate}</span>
-                   <span className="per-night"> per night</span>
-                   {this.getStars(this.props.avgRating)}
-               </div>
-               <div className="reserve-form-dates-guests">
-                   <h3 className="reserve-form-sub-header">Dates</h3>
-                   <DateRangePicker
-                      startDate={this.state.arrival_date}
-                      startDateId="arrival_date"
-                      endDate={this.state.departure_date}
-                      endDateId="depart_date"
-                      onDatesChange={({ startDate, endDate }) => this.setState({ departure_date: endDate, arrival_date: startDate })}
-                      focusedInput={this.state.focusedInput}
-                      onFocusChange={focusedInput => this.setState({ focusedInput })}
-                       numberOfMonths={num_months}
-                   />
-                  <h3 className="reserve-form-sub-header">Guests</h3>
-                    <select className="listing-show-guest-dropdown">
-                      <option value="1" default>1 guest</option>
-                      {number_of_guests}
-                    </select>
-               </div>
-               {this.showReservationDetails()}
-               <div className="error-text">{this.state.errors}</div>
-               <div className="success-text">{this.state.success}</div>
-                <button 
-                    className="pink-button wide-button"
-                    onClick={this.reserve}
-                >
-                    Reserve
-                </button>
-                {this.displayModal()}
-           </div>
-       )
+      const num_months = window.innerWidth <= 700 ? 1 : 2;
+        return (
+            <div className="reserve-form-container">
+                <div className="reserve-form-details">
+                    <span className="reserve-form-rate">${this.props.lair.rate}</span>
+                    <span className="per-night"> per night</span>
+                    {this.getStars(this.props.avgRating)}
+                </div>
+                <div className="reserve-form-dates-guests">
+                    <h3 className="reserve-form-sub-header">Dates</h3>
+                    <DateRangePicker
+                        startDate={this.state.arrival_date}
+                        startDateId="arrival_date"
+                        endDate={this.state.departure_date}
+                        endDateId="depart_date"
+                        onDatesChange={({ startDate, endDate }) => this.setState({ departure_date: endDate, arrival_date: startDate })}
+                        focusedInput={this.state.focusedInput}
+                        onFocusChange={focusedInput => this.setState({ focusedInput })}
+                        numberOfMonths={num_months}
+                        isDayBlocked={(day) => this.disableBookedDay(day)}
+                    />
+                    <h3 className="reserve-form-sub-header">Guests</h3>
+                      <select className="listing-show-guest-dropdown">
+                        <option value="1" default>1 guest</option>
+                        {number_of_guests}
+                      </select>
+                </div>
+                {this.showReservationDetails()}
+                <div className="error-text">{this.state.errors}</div>
+                <div className="success-text">{this.state.success}</div>
+                  <button 
+                      className="pink-button wide-button"
+                      onClick={this.reserve}
+                  >
+                      Reserve
+                  </button>
+                  {this.displayModal()}
+            </div>
+        )
    }
 }
 
